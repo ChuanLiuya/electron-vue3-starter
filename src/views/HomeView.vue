16854c667cbfcbaa931ac90668d9c1737e1b0389
chuan_liu_ya <chuan_liu_ya@163.com>
2026-08-11 10:19:37 +0800

    ✨ feat: 清理默认文件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 import CounterButton from '../components/CounterButton.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <CounterButton />
   </main>
 </template>
